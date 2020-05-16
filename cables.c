56fc9a1812bba1ad9b577a8ed073dfb50623e70f
Paul Partola <34845459+IAmScRay@users.noreply.github.com>
2020-05-17 02:12:09 +0300

    Add files via upload

---
+++ cables.c
@@ -0,0 +1,30 @@
+//
+//  cables.c
+//  GlobalLogic
+//
+//  Created by Paul Partola on 17.05.2020.
+//  Copyright © 2020 selfX Inc. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, const char * argv[]) {
+
+    int i, length = 0;
+    
+    if(argc != 3) {
+        printf("No or more than 2 arguments passed\n");
+        return -1;
+    }
+    
+    int N = atoi(argv[1]), D = atoi(argv[2]);
+    
+    for(i = 1; i <= N; i++) {
+        length += i * D;
+    }
+    
+    printf("%d\n", length);
+
+    return 0;
+}
